09bd6e19bc2f147552343dc629f96a39aab2f5bc
jorgeloaiza48 <jelm48@misena.edu.co>
2023-12-25 17:51:10 -0500

    desplegando

--- programador-backend/routes/olvidoPassword/olvidoPassword.js
+++ programador-backend/routes/olvidoPassword/olvidoPassword.js
@@ -29,11 +29,11 @@ router.post("/", (req, res) => {
                 }
                 token = jwt.sign(payload, JWT_SECRTET, { expiresIn: '5m' })
                                 
-                link = `http://localhost:3000/#/reset-password/${userFilter[0].id}/${token}`                                           
+                //link = `http://localhost:3000/#/reset-password/${userFilter[0].id}/${token}`                                           
                 //link = `https://programador-cursos.onrender.com/#/reset-password/${userFilter[0].id}/${token}`
 
                 URL = process.env.URL
-                //link = `${URL}/#/reset-password/${userFilter[0].id}/${token}`
+                link = `${URL}/#/reset-password/${userFilter[0].id}/${token}`
                 
 
                 // create reusable transporter object using the default SMTP transport               
@@ -44,8 +44,8 @@ router.post("/", (req, res) => {
                     secure: true,
                     auth: {
                         user: "jelm48@misena.edu.co",     //variable de entorno. Ver archivo ".env"
-                        //pass: process.env.PASSWORD  //variable de entorno. Ver archivo ".env"
-                        pass: "pzdl edyv smwk gugz"
+                        pass: process.env.PASSWORD  //variable de entorno. Ver archivo ".env"
+                        //pass: "pzdl edyv smwk gugz" //esta es la contraseña generada por google para apps en este caso "nodemailer"
                     }
                 })
                 //Features of email to be sent
